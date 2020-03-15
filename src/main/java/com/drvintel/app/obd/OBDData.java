f6fa2f44a2ebf500f339f00b2f199db71fca026f
Ayush Raj <ayush221294@gmail.com>
2020-03-15 19:53:51 +0530

    ARchanges

--- src/main/java/com/drvintel/app/obd/OBDData.java
+++ src/main/java/com/drvintel/app/obd/OBDData.java
@@ -15,6 +15,42 @@ public class OBDData {
 	@EmbeddedId
 	private OBDDataPK obdDevicePK;
 	@Column
-	private Double speed; 
+	private String Veh_Cal_Eng_Load;
+	@Column
+	private String Veh_Eng_Coolant_Temp;
+	@Column
+	private String Veh_Short_Term_Fuel_Trim_Bank_1;
+	@Column
+	private String Veh_Long_Term_Fuel_Trim_Bank_1;
+	@Column
+	private String Veh_Short_Term_Fuel_Trim_Bank_2;
+	@Column
+	private String Veh_Engine_RPM;
+	@Column
+	private String Veh_Speed;
+	@Column
+	private String Veh_Intake_Air_Temp;
+	@Column
+	private String Veh_Throttle_Position;
+	@Column
+	private String Veh_Fuel_Tank_Level_Input;
+	@Column
+	private String Veh_Dist_Trav_Since_Cod_Cleared;
+	@Column
+	private String Veh_Rela_Throt_Pos;
+	@Column
+	private String Veh_Amb_Air_Temp;
+	@Column
+	private String Veh_Abs_Throt_Pos_B;
+	@Column
+	private String Veh_Abs_Throt_Pos_C;
+	@Column
+	private String Veh_Abs_Throt_Pos_D;
+	@Column
+	private String Veh_Abs_Throt_Pos_E;
+	@Column
+	private String Veh_Abs_Throt_Pos_F;
+	@Column
+	private String Veh_Fuel_Type; 
 
 }
