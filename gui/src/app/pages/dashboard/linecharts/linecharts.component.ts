254fccaf10dd29c58d1aaeaf9450e5c12b63d6b3
swatithapa19 <swatithapa019@gmail.com>
2020-05-04 18:39:01 +0530

    Dummy graph from JSON

--- gui/src/app/pages/dashboard/linecharts/linecharts.component.ts
+++ gui/src/app/pages/dashboard/linecharts/linecharts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { LinechartsService } from './linecharts.service';
+import { Iloc} from './line';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'ngx-linecharts',
@@ -18,12 +20,49 @@ styles:[]*/
 })
 export class LinechartsComponent implements OnInit {
     public lines =[];
-
-  constructor(private _linechartsService: LinechartsService) { }
-
-  ngOnInit() {
-      this._linechartsService.getline()
-        .subscribe(data => this.lines = data);
     
+   
+   constructor(private _linechartsService: LinechartsService) { }
+  
+
+  ngOnInit(): void {
+   this._linechartsService.getline()
+     .subscribe(data => this.lines = data);
+ 
+     const xlabels =[];
+     const ycases =[];
+     ChatIt();
+      async function ChatIt() {
+        await getData();
+        var myChart = new Chart("myChart", {
+        type: 'line',
+        data: {
+          labels: xlabels,
+          datasets: [{
+              label: 'Coronavirus cases',
+              data: ycases,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+          }]
+  },
+})
+};
+    getData();
+    async function getData() {
+      const response = await fetch("/assets/data/line.json");
+      const data = await response.text();
+
+      const table = data.split('\n').slice(1);
+      console.log(table);
+      table.forEach(row => {
+        const columns = row.split(',');
+        const loc = columns[0];
+        xlabels.push(loc);
+        const cases = columns[1];
+        ycases.push(cases);
+        console.log(loc,cases);
+      });
+    }
 }
 }
