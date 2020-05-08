94cd67c4a74b441643b85a4e25a69ebeef0b5a60
swatithapa19 <swatithapa019@gmail.com>
2020-05-08 16:13:32 +0530

    Dashboard and searchfield component add in UI module.ts

--- drvintelgui/src/app/pages/ui-features/ui-features.module.ts
+++ drvintelgui/src/app/pages/ui-features/ui-features.module.ts
@@ -4,11 +4,15 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { SpeedChartComponent } from './speed/speed.component';
 import { UiFeaturesRoutingModule } from './ui-features-routing.module';
 import { UiFeaturesComponent } from './ui-features.component';
+import { SearchComponent } from './search-fields/search-fields.component';
+import { DashboardComponent }from './dashboard/dashboard.component';
 
 
 const components = [
   UiFeaturesComponent,
   SpeedChartComponent,
+  SearchComponent,
+  DashboardComponent
 ];
 
 @NgModule({
