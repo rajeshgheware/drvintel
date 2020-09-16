d3a277e7934d7cb27b64246f56a692c6d733cd0d
atulsrao <atulsrao98@gmail.com>
2020-09-16 18:57:27 +0530

    Aman's Code

---
+++ angular/node_modules/@amcharts/amcharts4/.internal/core/Options.js
@@ -0,0 +1,20 @@
+/**
+ * Global options.
+ */
+export var options = {
+    verbose: true,
+    commercialLicense: false,
+    classNamePrefix: "amcharts-",
+    autoSetClassName: false,
+    minPolylineStep: 0.5,
+    onlyShowOnViewport: false,
+    queue: false,
+    autoDispose: false,
+    licenses: [],
+    suppressErrors: false,
+    suppressWarnings: false,
+    animationsEnabled: true,
+    nonce: "",
+    deferredDelay: 100
+};
+//# sourceMappingURL=Options.js.map
